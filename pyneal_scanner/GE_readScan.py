fce4f2ae45a1953ee17873cf6c3ab30a10039e98
jeffmacinnes <jeff.macinnes@gmail.com>
2017-10-02 16:42:22 -0700

    added thread for processing and removing slices from the Queue

--- pyneal_scanner/GE_readScan.py
+++ pyneal_scanner/GE_readScan.py
@@ -33,10 +33,10 @@ class GE_scanRead(Thread):
         # initialize class parameters
         self.interval = interval            # interval for looping the thread
         self.seriesDir = seriesDir          # full path to series directory
-        self.dicomQ = dicomQ        # queue to store dicom files
+        self.dicomQ = dicomQ                # queue to store dicom files
         self.alive = True                   # thread status
+        self.num_slicesAdded = 0            # counter to keep track of # of slices added overall
         self.dicom_files = set()            # initialize empty set to store dicoms
-
 
     def run(self):
         # function that loops while the Thead is still alive
@@ -47,11 +47,12 @@ class GE_scanRead(Thread):
 
             # figure out which haven't already been added to the queue
             newDicoms = [f for f in currentDicoms if f not in self.dicom_files]
-            print('found {} new dicoms'.format(len(newDicoms)))
 
             # add all of the new dicoms to the
             for f in newDicoms:
                 self.dicomQ.put(f)
+            print('Put {} new slices on the queue'.format(len(newDicoms)))
+            self.num_slicesAdded += len(newDicoms)
 
             # now update the set of dicoms added to the queue
             self.dicom_files.update(set(newDicoms))
@@ -59,10 +60,8 @@ class GE_scanRead(Thread):
             # pause
             time.sleep(self.interval)
 
-
-    def nSlicesAdded(self):
-        return self.dicomQ.qsize()
-
+    def get_nSlicesAdded(self):
+        return self.num_slicesAdded
 
     def stop(self):
         # function to stop the Thread
@@ -70,6 +69,38 @@ class GE_scanRead(Thread):
         self.alive = False
 
 
+class GE_processSlice(Thread):
+    """
+    Class to process each dicom slice in the dicom queue
+    """
+    def __init__(self, dicomQ):
+        # start the thread upon creation
+        Thread.__init__(self)
+
+        # initialize class parameters
+        self.dicomQ = dicomQ
+        self.alive = True
+        print('processSlice Started....')
+
+    def run(self):
+        # function to run on loop
+        while self.alive:
+
+            # if there are any slices in the queue, process them
+            if not self.dicomQ.empty():
+                self.num_slicesInQueue = self.dicomQ.qsize()
+                print('There are {} items in queue'.format(self.num_slicesInQueue))
+                for s in range(self.num_slicesInQueue):
+                    thisFile = self.dicomQ.get(True, 2)  # block for up to 2 seconds
+                    print('Removed {} from queue'.format(thisFile))
+                    self.dicomQ.task_done()  # complete the task on this item in the queue
+
+            time.sleep(.5)
+
+    def stop(self):
+        # function to stop the Thread
+        print('Stopped processing slices')
+        self.alive = False
 
 
 def startScanRead(sessionDir):
@@ -78,36 +109,42 @@ def startScanRead(sessionDir):
     a queue to store incoming dicom files, and start listening!
     """
 
-    # initialize the dicom queue
+    # initialize the dicom queue to keep track of dicom slice files
     dicomQ = Queue()
 
     # wait for series directory to be created
     seriesDir = waitForSeriesDir(sessionDir)
 
+    #### Start threads to listen for new slices,
+    #### and to process each slice
     # create an instance of the class to monitor for GE-style scans
     scanRead = GE_scanRead(seriesDir, dicomQ)
     scanRead.start()
+
+    # create an instance of the class to process each slice
+    processSlice = GE_processSlice(dicomQ)
+    processSlice.start()
 
 
     # TMP -- for testing
     keepListening = True
     while keepListening:
-        nSlicesAdded = scanRead.nSlicesAdded()
-        if nSlicesAdded >= 10:
+        nSlicesAdded = scanRead.get_nSlicesAdded()
+        if nSlicesAdded >= 100:
             print('Got enough slices: {}'.format(nSlicesAdded))
             scanRead.stop()
+            scanRead.join()     # wait til all tasks on this thread are complete
 
-            dicomQ.join()      # blocks until all tasks complete
-            scanRead.join()
+            processSlice.stop()
+            processSlice.join() # wait til all tasks on this thread are complete
+
+            #dicomQ.join()      # blocks until all tasks complete
+            scanRead.join()     # wait for the thread to exit
 
             keepListening = False
 
         #
         time.sleep(.5)
-
-
-
-
 
 
 
