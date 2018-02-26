0c0ad4f0367aa5bdda48baaf9b3e9db384cabd07
Jeff MacInnes <jeff.macinnes@gmail.com>
2018-02-26 00:21:10 -0800

    adding functions to createMask GUI

--- src/GUIs/createMask/createMaskGUI.py
+++ src/GUIs/createMask/createMaskGUI.py
@@ -9,7 +9,7 @@ that file to obtain initial settings, and then once the user hits 'submit' the
 file is overwritten with new settings
 """
 import os
-from os.path import join
+from os.path import join, dirname
 import sys
 
 import yaml
@@ -24,15 +24,18 @@ from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.popup import Popup
 from kivy.factory import Factory
 
-
-
 # Set Window Size
 from kivy.config import Config
-Config.set('graphics', 'width', '400')
-Config.set('graphics', 'height', '500')
+Config.set('graphics', 'width', '450')
+Config.set('graphics', 'height', '600')
 
 # initialize global var that will store path to createMaskConfig.yaml file
 createMaskConfigFile = None
+
+# global paths
+setupGUI_dir = os.path.dirname(os.path.abspath(__file__))
+pynealDir = dirname(dirname(dirname(setupGUI_dir)))
+
 
 class FilePathInputField(TextInput):
     pass
@@ -113,8 +116,87 @@ class MainContainer(BoxLayout):
     # settings for the GUI.
     GUI_settings = DictProperty({}, rebind=True)
     setupGUI_dir = os.path.dirname(os.path.abspath(__file__))
-    print(setupGUI_dir)
+    MNI_standards_dir = join(pynealDir, 'utils/MNI_templates')
+
     textColor = ListProperty([0,0,0,1])
+    disabledTextColor = ListProperty([.6, .6, .6, 1])
+
+    def __init__(self, **kwargs):
+
+        # self.MNI_standards_dir = join(pynealDir, 'utils/MNI_templates')
+
+        self.GUI_settings = self.readSettings(createMaskConfigFile)
+
+
+        # pass the keywords along to the parent class
+        super().__init__(**kwargs)
+
+    ### Methods for dealing with loading/saving Settings -----------------------
+    def readSettings(self, settingsFile):
+        """
+        Open the supplied settingsFile, and compare to the default
+        values. Any valid setting in the settingsFile will override
+        the default
+        """
+        # set up defaults
+        defaultSettings = {
+            'subjFunc': ['', str],
+            'createFuncBrainMask': [True, bool],
+            'transformMaskToFunc': [False, bool],
+            'subjAnat': ['', str],
+            'MNI_standard': [join(self.MNI_standards_dir, 'MNI152_T1_1mm_brain.nii.gz'), str],
+            'MNI_mask': ['', str],
+            'outputPrefix': ['test', str]
+        }
+
+        # initialize dictionary that will eventually hold the new settings
+        newSettings = {}
+
+        # load the settingsFile, if it exists and is not empty
+        if os.path.isfile(settingsFile) and os.path.getsize(settingsFile) > 0:
+            # open the file, load all settings from the file into a dict
+            with open(settingsFile, 'r') as ymlFile:
+                loadedSettings = yaml.load(ymlFile)
+
+            # Go through all default settings, and see if there is
+            # a loaded setting that should overwrite the default
+            for k in defaultSettings.keys():
+                # does this key exist in the loaded settings
+                if k in loadedSettings.keys():
+                    loadedValue = loadedSettings[k]
+
+                    # does the dtype of the value match what is
+                    # specifed by the default?
+                    if type(loadedValue) == defaultSettings[k][1]:
+                        newSettings[k] = loadedValue
+                    else:
+                        # throw error and quit
+                        print('Problem loading the settings file!')
+                        print('{} setting expecting dtype {}, but got {}'.format(
+                                k,
+                                defaultSettings[k][1],
+                                type(loadedValue)
+                                ))
+                        sys.exit()
+                # if the loaded file doesn't have this setting, take the default
+                else:
+                    newSettings[k] = defaultSettings[k][0]
+
+        # if no settings file exists, use the defaults
+        else:
+            for k in defaultSettings.keys():
+                newSettings[k] = defaultSettings[k][0]
+
+        # return the settings dict
+        return newSettings
+
+    def setCreateFuncBrainMask(self):
+        self.GUI_settings['createFuncBrainMask'] = self.ids.createFuncBrainMaskCheckbox.active
+
+
+    def submitGUI(self):
+        print('submit button pressed')
+        print(self.ids.subjFunc.currentPath)
 
 
     ### Show Notification Pop-up ##############################################
