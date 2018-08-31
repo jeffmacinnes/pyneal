5fd10b06532cc951e5e68f032ad453a514e50122
Jeff MacInnes <jeff.macinnes@gmail.com>
2018-08-31 15:07:35 -0700

    started test scanReceiver

--- tests/pyneal_tests/test_scanReceiver.py
+++ tests/pyneal_tests/test_scanReceiver.py
@@ -4,6 +4,7 @@ import sys
 import socket
 import json
 
+import zmq
 import numpy as np
 
 import helper_tools
@@ -11,7 +12,10 @@ import helper_tools
 # get dictionary with relevant paths for tests within this module
 paths = helper_tools.get_pyneal_scanner_test_paths()
 sys.path.insert(0, paths['pynealDir'])
-pynealScannerPort = 5555
+
+port = 5555
+host = '127.0.0.1'
+
 
 from src.scanReceiver import ScanReceiver
 
@@ -20,11 +24,24 @@ def test_resultsServer():
     """ tests pyneal.src.resultsServer """
 
     # create settings dictionary
-    settings = {'pynealScannerPort': pynealScannerPort,
+    settings = {'pynealScannerPort': port,
+                'pynealHost': host,
                 'numTimepts': 3,
-                'launchDashboard': False}
+                'launchDashboard': False,
+                'seriesOutputDir': paths['testDataDir']}
     scanReceiver = ScanReceiver(settings)
     scanReceiver.daemon = True
     scanReceiver.start()
 
-    
+    # Simulate Pyneal Scanner making a connection to the scanReceiver
+    context = zmq.Context.instance()
+    socket = context.socket(zmq.PAIR)
+    socket.connect('tcp://{}:{}'.format(host, port))
+
+    while True:
+        msg = 'hello from test pyneal scanner simulator'
+        socket.send_string(msg)
+
+        resp = socket.recv_string()
+        if resp == msg:
+            break
