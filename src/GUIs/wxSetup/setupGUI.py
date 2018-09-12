01a7a964e8801aa07d7fcfeaeec671f25b9036f1
jeffmacinnes <jeff.macinnes@gmail.com>
2018-09-11 19:28:51 -0700

    wx GUI structure

--- src/GUIs/wxSetup/setupGUI.py
+++ src/GUIs/wxSetup/setupGUI.py
@@ -1,45 +1,113 @@
+import os
+from os.path import join
+
 import wx
-print(wx.__version__)
-pynealColor = '#C74053'
+
+pynealColor = '#B04555'
 
 
 class SetupFrame(wx.Frame):
     def __init__(self, parent, title="Pyneal Setup"):
         super(SetupFrame, self).__init__(parent, title=title)  #initialize the parent class
+        self.setupGUI_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # initialize all gui panels and settings
         self.InitUI()
 
     def InitUI(self):
+        """ Initialize all GUI windows and widgets """
+
+        self.setupPanel = wx.Panel(self, -1)
+        self.setupPanel.SetBackgroundColour("White")
+        self.headerFont = wx.Font(wx.FontInfo(22).FaceName('Helvetica').Bold().AntiAliased(True))
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
         self.Centre()
 
         # create the panels
-        self.createLogoPanel()
-        self.createCommunicationPanel()
-        # self.createMaskPanel()
-        # self.createPreprocessingPanel()
-        # self.createAnalysisPanel()
-        # self.createOutputPanel()
-        # self.createSubmitPanel()
+        logoSizer = self.createLogoBox()
+        commSizer = self.createCommunicationBox()
+        preprocSizer = self.createPreprocessingBox()
+
+        vbox.Add(logoSizer, flag=wx.EXPAND | wx.ALL, border=10, proportion=1)
+        vbox.Add(commSizer, flag=wx.EXPAND | wx.ALL, border=10, proportion=1)
+        vbox.Add(preprocSizer, flag=wx.EXPAND | wx.ALL, border=10, proportion=1)
+
+        # self.createMaskBox()
+        # self.createPreprocessingBox()
+        # self.createAnalysisBox()
+        # self.createOutputBox()
+        # self.createSubmitBox()
+        self.setupPanel.SetSizer(vbox)
+        vbox.Fit(self)
 
     ### (VIEW) - Panel creation methods ---------------------------------------
-    def createLogoPanel(self):
-        pass
+    def createLogoBox(self):
+        """ draw the top logo panel """
+        # top level sizer for this panel
+        logoSizer = wx.BoxSizer(wx.HORIZONTAL)  # toplevel sizer for this box
 
-    def createCommunicationPanel(self):
-        pass
+        # add the logo and text images
+        logoJPG = wx.Image(join(self.setupGUI_dir, 'images/pynealLogo_200w.jpg'), wx.BITMAP_TYPE_ANY)
+        logoImg = wx.StaticBitmap(self.setupPanel, -1, logoJPG.ConvertToBitmap())
+
+        textJPG = wx.Image(join(self.setupGUI_dir, 'images/pynealText_200w.jpg'), wx.BITMAP_TYPE_ANY)
+        textImg = wx.StaticBitmap(self.setupPanel, -1, textJPG.ConvertToBitmap())
+
+        # add images to the toplevel sizer for this panel
+        logoSizer.Add(logoImg, flag=wx.LEFT, proportion=0)
+        logoSizer.Add(textImg, flag=wx.LEFT, proportion=0)
+
+        return logoSizer
+
+    def createCommunicationBox(self):
+        """ draw the Communications box """
+        commSizer = wx.BoxSizer(wx.VERTICAL)    # toplevel sizer for this box
+
+        # add  header for this box
+        headerImg = self.drawHeader(label='Communication')
+        commSizer.Add(headerImg, flag=wx.EXPAND | wx.TOP, proportion=0)
+        #commSizer.Add(label, flag=wx.EXPAND | wx.TOP, border=5, proportion=0)
+
+        return commSizer
+
+    def createPreprocessingBox(self):
+        """ draw the Preprocessing Box """
+        preprocSizer = wx.BoxSizer(wx.VERTICAL)    # toplevel sizer for this box
+
+        # add  header for this box
+        headerImg = self.drawHeader(label='Preprocessing')
+        preprocSizer.Add(headerImg, flag=wx.EXPAND | wx.TOP, proportion=0)
+
+        return preprocSizer
+
 
     ### (CONTROL) - Event Handling and User Interaction -----------------------
+    # def OnPaint(self, e):
+    #     for hp in self.headerPanels:
+    #         self.drawHeader(hp)
 
-    def OnMove(self, e):
-        x, y = e.GetPosition()
-        self.st1.SetLabel(str(x))
-        self.st2.SetLabel(str(y))
+    def drawHeader(self, label="None"):
+        headerJPG = wx.Image(join(self.setupGUI_dir, 'images/headerBG.jpg'), wx.BITMAP_TYPE_ANY)
+
+        bmp = wx.Image(join(self.setupGUI_dir, 'images/headerBG.jpg'), wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        dc = wx.MemoryDC()
+        dc.SelectObject(bmp)
+        dc.SetTextForeground("white")
+        dc.SetFont(self.headerFont)
+        dc.DrawText(label, 15, 15)
+
+        headerImg = wx.StaticBitmap(self.setupPanel, -1, bmp)
+        return headerImg
+
 
 
 class SetupApp(wx.App):
     """ Application class for setup GUI """
 
     def OnInit(self):
-        self.frame = SetupFrame(None) #, title='fuck you')
+        self.frame = SetupFrame(None, title='Pyneal Setup')
         self.frame.Show()
         self.SetTopWindow(self.frame)
         return True
